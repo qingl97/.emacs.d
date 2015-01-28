58da2cc83d7b8f942c8ef369364d5c67b57c2292
Qing LIANG <qingl97@gmail.com>
2015-01-28 19:32:14 +0100

    add yasnippet, google-c-style, auto-complete-c-headers

--- init.el
+++ init.el
@@ -1,16 +1,35 @@
+;; start package.el with emacs
 (require 'package)
 (add-to-list 'package-archives
              '("melpa" . "http://melpa.org/packages/") t)
+;; initialize package.el
 (package-initialize)
 
-;;(add-to-list 'load-path "~/.emacs.d")
-;;(require 'auto-complete-config)
-;;(ac-config-default)
-;;(add-to-list 'ac-dictionary-directories "~/.emacs.d/ac-dict")
-
+;; start auto-complete with emacs
 (require 'auto-complete)
 (global-auto-complete-mode t)
+;; do default config for auto-complete
+(require 'auto-complete-config)
+(ac-config-default)
 
+;; set emacs indentation
 (setq tab-width 4) ; or any other preferred value
 (defvaralias 'c-basic-offset 'tab-width)
 (defvaralias 'cperl-indent-level 'tab-width)
+
+; start yasnippet with emacs
+(require 'yasnippet)
+(yas-global-mode 1)
+
+;; let's define a function which initializes auto-complete-c-headers and gets called for c/c++ hooks
+(defun my:ac-c-header-init ()
+  (require 'auto-complete-c-headers)
+  (add-to-list 'ac-sources 'ac-source-c-headers)
+  (add-to-list 'achead:include-directories '"/Applications/Xcode.app/Contents/Developer/Toolchains/XcodeDefault.xctoolchain/usr/bin/../lib/clang/6.0/include")
+)
+;; now let's call this function from c/c++ hooks
+(add-hook 'c++-mode-hook 'my:ac-c-header-init)
+(add-hook 'c-mode-hook 'my:ac-c-header-init)
+
+;; Fix iedit bug in Mac
+(define-key global-map (kbd "C-c ;") 'iedit-mode)
